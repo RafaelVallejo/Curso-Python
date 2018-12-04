e79c2231362479bb94b3334b100853ca84f69a18
RafaelVallejo <45380391+RafaelVallejo@users.noreply.github.com>
2018-12-04 12:26:48 -0600

    Update tarea6.py

--- tarea6.py
+++ tarea6.py
@@ -12,7 +12,7 @@ session = session()
 
 def printError(msg, exit = False):
     """Función que imprime mensaje de error y sale del programa
-    Recibe: mensage a mostrar y booleano que indica si se debe terminar la ejecución del programa"""
+    Recibe: mensaje a mostrar y booleano que indica si se debe terminar la ejecución del programa"""
     sys.stderr.write('Error:\t%s\n' % msg)
     if exit:
         sys.exit(1)
