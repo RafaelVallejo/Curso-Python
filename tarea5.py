a4d96da4287503fa4cccd6ec4027e475beb01992
RafaelVallejo <45380391+RafaelVallejo@users.noreply.github.com>
2018-12-04 12:27:07 -0600

    Update tarea5.py

--- tarea5.py
+++ tarea5.py
@@ -10,7 +10,7 @@ from requests.exceptions import ConnectionError
 
 def printError(msg, exit = False):
     """Función que imprime mensaje de error y sale del programa
-        Recibe: mensage a mostrar y booleano que indica si se debe terminar la ejecución del programa"""
+        Recibe: mensaje a mostrar y booleano que indica si se debe terminar la ejecución del programa"""
     sys.stderr.write('Error:\t%s\n' % msg)
     if exit:
         sys.exit(1)
